a1aa1b7c254c96dc328b4cd530e03d58a022bc91
isaacreisbr23 <66951257+isaacreisbr23@users.noreply.github.com>
2021-10-17 00:40:45 -0300

    Update detectIps.sh
    concertando o código

--- detectIps.sh
+++ detectIps.sh
@@ -6,12 +6,16 @@ tryRun
 
 function tryRun(){
 if ["$1" == ""]
+then
 echo "Erro deseja rodar novamente? y/n"
 read response
 if ["$response" == "y"]
+then
 runAgain
 else
 echo "finalizado"
 fi
 else
 echo "teste ocorreu"
+fi
+tryRun
